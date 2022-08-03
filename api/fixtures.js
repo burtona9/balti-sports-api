2ff02f0a71b7cc5d55697a40be8139ae1fe1c48e
burtona9 <adamburton27@hotmail.com>
2022-08-03 17:50:50 +0100

    initial commit

---
+++ api/fixtures.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', async (req,res) => {
+    try {
+        
+        res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+
+        res.json({
+            status:200,
+            message: 'Got data successfully',
+        });
+    } catch (error){
+        console.error(error);
+        return res.status(500).send('Server error');
+
+    
+    }   
+})
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const fixtures = require('./api/fixtures');
+
+app.use(express.json({extended:false}));
+
+app.use('/api/fixtures',fixtures);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen( PORT, ()=> console.log(`server is running on ${PORT}`));
