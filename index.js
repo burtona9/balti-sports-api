bc6e0a675118defacc98a2dd097ce678588cbcd9
burtona9 <adamburton27@hotmail.com>
2022-08-03 19:54:32 +0100

    test

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const fixtures = require('./public/api/fixtures');
 app.use(express.json({extended:false}));
 
 app.use('/api/fixtures',fixtures);
-
 const PORT = process.env.PORT || 8080;
 
 app.listen( PORT, ()=> console.log(`server is running on ${PORT}`));
